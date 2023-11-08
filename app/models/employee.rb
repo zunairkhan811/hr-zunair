13d4d23a56a7b06e4fa684adb62a65cca2f845a0
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-08 10:04:37 +0500

    use of crud operation on active record

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+    validates :first_name, :last_name, presence: true
+    validates :email, presence:true, uniqueness: true
+    validates :city, :state, :country, :zipcode, :address_line_1, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# employee = Employee.create(
+#     first_name: 'Muhammad Zunair',
+#     last_name: 'Khan',
+#     email: 'zunairkhan811@gmail.com',
+#     city: 'Multan',
+#     state: 'Punjab',
+#     country: 'Pakistan',
+#     zipcode: '70000',
+#     address_line_1: 'House No. 1122, near suraj miani'
+# )
+
+# employee = Employee.new(
+#     first_name: 'Muhammad Zunair',
+#     last_name: 'Khan',
+#     email: 'employee2@gmail.com',
+#     city: 'Multan',
+#     state: 'Punjab',
+#     country: 'uk',
+#     zipcode: '70000',
+#     address_line_1: 'House No. 2211, near suraj miani'
+# )
+# employee.save()
