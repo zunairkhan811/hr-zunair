1103b3493e6083215b070ffef8d0ae9f8845d0e2
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-08 18:25:43 +0500

    add resource controller and show dbdata on browser

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,11 @@ class Employee < ApplicationRecord
     validates :first_name, :last_name, presence: true
     validates :email, presence:true, uniqueness: true
     validates :city, :state, :country, :zipcode, :address_line_1, presence: true
+
+    def name
+        "#{first_name} #{last_name}".strip
+    end
+    def full_address
+        "#{address_line_1} #{city}, #{state}, #{country}, #{zipcode}".strip
+    end
 end
