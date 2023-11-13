a7e39fee00de5bdb3637851372571cc9e84e9af7
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-13 18:26:57 +0500

    display dropdown for doctypes

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,7 @@
 class Document < ApplicationRecord
+
+  DOC_TYPES = ['Marksheet', 'Photo', 'CNIC', 'Address Proof', 'Others'].freeze
+
   belongs_to :employee
   validates :name, :doc_type, presence:true
 end
