efffbcccaecd0a3dea6e1567ff886b0f31d4899e
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-13 15:56:02 +0500

    Make of document model with employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
+
+    has_many :documents
+    
     validates :first_name, :last_name, presence: true
     validates :email, presence:true, uniqueness: true
     validates :city, :state, :country, :zipcode, :address_line_1, presence: true
