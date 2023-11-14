6b33925c8e2117b083547f66633002fac4ce5b2c
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-14 10:35:06 +0500

    display employee dropdown for document model attachments:

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
 
     has_many :documents
-    
+
     validates :first_name, :last_name, presence: true
     validates :email, presence:true, uniqueness: true
     validates :city, :state, :country, :zipcode, :address_line_1, presence: true
@@ -11,5 +11,8 @@ class Employee < ApplicationRecord
     end
     def full_address
         "#{address_line_1}, #{address_line_2} #{city}, #{state}, #{country}, #{zipcode}".strip
+    end
+    def name_with_email
+        "#{name}(#{email})"
     end
 end
