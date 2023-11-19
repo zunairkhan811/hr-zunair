348261e963fd5a4cbb913309c2b02b1039ad729b
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-19 16:28:31 +0500

    add columns to existing employee model and created migration for that and show that columns fields on browser and put validation on them

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,6 +5,7 @@ class Employee < ApplicationRecord
     validates :first_name, :last_name, presence: true
     validates :email, presence:true, uniqueness: true
     validates :city, :state, :country, :zipcode, :address_line_1, presence: true
+    validates :date_of_birth, :date_of_joining, :job_title, presence: true
 
     def name
         "#{first_name} #{last_name}".strip
