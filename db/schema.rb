2b1f965a6973c9401c31876775dd25830012ffab
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-19 17:57:10 +0500

    rename column through migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_19_110519) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_19_125010) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_19_110519) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
-    t.string "middle_name"
     t.string "last_name"
     t.string "email"
     t.string "city"
@@ -64,7 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_19_110519) do
     t.date "date_of_birth"
     t.string "job_title"
     t.date "date_of_joining"
-    t.text "bio"
+    t.text "about"
+    t.string "middle_name"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
