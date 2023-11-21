d183a418dc0475e6a1712e320d7c66eb8293532e
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-11-21 22:58:43 +0500

    change view for password and edit password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations' 
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations' 
   }
   root 'home#index'
 
